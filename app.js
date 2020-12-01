ed9c3fbb99db623f8c18c28e7d6fde805130f74f
Yana <masatomo.yanagihara.jp@gmail.com>
2020-12-01 15:48:03 +0900

    Fix: クエリパラメータによってフィルタリング

--- app.js
+++ app.js
@@ -7,5 +7,14 @@ let todos = [
 ];
 
 const app = express();
-app.get("/api/todos", (req, res) => res.json(todos));
+
+// ToDo一覧の取得
+app.get("/api/todos", (req, res) => {
+  if (!req.query.completed) {
+    return res.json(todos);
+  }
+  // completedクエリパラメータを指定された場合はToDoをフィルタリング
+  const completed = req.query.completed === "true";
+  res.json(todos.filter((todo) => todo.completed === completed));
+});
 app.listen(3000);

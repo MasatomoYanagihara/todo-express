b09c62540cd5bb887985d92f3ae6cd9afbe54999
Yana <masatomo.yanagihara.jp@gmail.com>
2020-12-01 17:46:38 +0900

    Add: ToDoの新規登録処理

--- app.js
+++ app.js
@@ -8,6 +8,8 @@ let todos = [
 
 const app = express();
 
+app.use(express.json());
+
 // ToDo一覧の取得
 app.get("/api/todos", (req, res) => {
   if (!req.query.completed) {
@@ -17,4 +19,30 @@ app.get("/api/todos", (req, res) => {
   const completed = req.query.completed === "true";
   res.json(todos.filter((todo) => todo.completed === completed));
 });
+
+// ToDoのIDの値を管理する為の変数
+let id = 2;
+
+// ToDoの新規登録
+app.post("/api/todos", (req, res, next) => {
+  const { title } = req.body;
+  if (typeof title !== "string" || !title) {
+    // titleがリクエストに含まれない場合はステータスコード400(Bad Request)
+    const err = new Error("title is required");
+    err.statusCode = 400;
+    return next(err);
+  }
+  // ToDoの作成
+  const todo = { id: (id += 1), title, completed: false };
+  todos.push(todo);
+  // ステータスコード201(Created)で結果を返す
+  res.status(201).json(todo);
+});
+
+// エラーハンドリングミドルウェア
+app.use((err, req, res, next) => {
+  console.log(err);
+  res.status(err.statusCode || 500).json({ error: err.message });
+});
+
 app.listen(3000);

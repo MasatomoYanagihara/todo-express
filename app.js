e399445e86e43df27cecfdf4d1bf73d09c76c462
Yana <masatomo.yanagihara.jp@gmail.com>
2020-12-03 18:34:50 +0900

    ７章開始

--- app.js
+++ app.js
@@ -1,96 +1,76 @@
 "use strict";
-const express = require("express");
 
-let todos = [
-  { id: 1, title: "ネーム", completed: false },
-  { id: 2, title: "下書き", completed: true },
-];
+const express = require("express");
+const { v4 } = require("uuid");
+const uuidv4 = v4;
+
+// 実行されたスクリプトの名前に応じてデータストレージの実装を使い分ける
+// npm startなら「process.env.npm_lifecycle_event」はstartになる
+const dataStorage = require(`./${process.env.npm_lifecycle_event}`);
 
 const app = express();
 
 app.use(express.json());
 
 // ToDo一覧の取得
-app.get("/api/todos", (req, res) => {
+app.get("/api/todos", (req, res, next) => {
   if (!req.query.completed) {
-    return res.json(todos);
+    return dataStorage.fetchAll().then((todos) => res.json(todos), next);
   }
-  // completedクエリパラメータを指定された場合はToDoをフィルタリング
   const completed = req.query.completed === "true";
-  res.json(todos.filter((todo) => todo.completed === completed));
+  dataStorage
+    .fetchByCompleted(completed)
+    .then((todos) => res.json(todos), next);
 });
-
-// 全クライアントに対するSSE送信関数を保持する配列
-let sseSenders = [];
-
-// SSEのIDを管理するための変数
-let sseId = 1;
-
-// ToDo一覧の取得（SSE）
-app.get("/api/todos/events", (req, res) => {
-  // タイムアウトを抑止
-  req.socket.setTimeout(0);
-  res.set({
-    // Content-TypeでSSEであることを示す
-    "Content-Type": "text/event-stream",
-  });
-  // クライアントにSSEを送信する関数を作成して登録
-  const send = (id, data) => res.write(`id: ${id}\ndata: ${data}\n\n`);
-  sseSenders.push(send);
-  // リクエスト発生時点の状態を送信
-  send(sseId, JSON.stringify(todos));
-  // リクエストがクローズされたらレスポンスを終了してSSE送信関数を配列から削除
-  req.on("close", () => {
-    res.end();
-    sseSenders = sseSenders.filter((_send) => _send !== send);
-  });
-});
-
-// ToDoの更新に伴い、全クライアントに対してSSEを送信する
-function onUpdateTodos() {
-  sseId += 1;
-  const data = JSON.stringify(todos);
-  sseSenders.forEach((send) => send(sseId, data));
-}
-
-// ToDoのIDの値を管理する為の変数
-let id = 2;
 
 // ToDoの新規登録
 app.post("/api/todos", (req, res, next) => {
   const { title } = req.body;
   if (typeof title !== "string" || !title) {
-    // titleがリクエストに含まれない場合はステータスコード400(Bad Request)
+    // titleがリクエストに含まれない場合はステータスコード400（Bad Request）
     const err = new Error("title is required");
     err.statusCode = 400;
     return next(err);
   }
-  // ToDoの作成
-  const todo = { id: (id += 1), title, completed: false };
-  todos.push(todo);
-  // ステータスコード201(Created)で結果を返す
-  res.status(201).json(todo);
-  onUpdateTodos();
+  const todo = { id: uuidv4() /* UUIDの生成 */, title, completed: false };
+  dataStorage.create(todo).then(() => res.status(201).json(todo), next);
 });
+
+/** Completedの設定、解除の共通処理 */
+function completedHandler(completed) {
+  return (req, res, next) =>
+    dataStorage.update(req.params.id, { completed }).then((todo) => {
+      if (todo) {
+        return res.json(todo);
+      }
+      const err = new Error("ToDo not found");
+      err.statusCode = 404;
+      next(err);
+    }, next);
+}
+
+// ToDoのCompletedの設定、解除
+app
+  .route("/api/todos/:id/completed")
+  .put(completedHandler(true))
+  .delete(completedHandler(false));
+
+// ToDoの削除
+app.delete("/api/todos/:id", (req, res, next) =>
+  dataStorage.remove(req.params.id).then((id) => {
+    if (id !== null) {
+      return res.status(204).end();
+    }
+    const err = new Error("ToDo not found");
+    err.statusCode = 404;
+    next(err);
+  }, next)
+);
 
 // エラーハンドリングミドルウェア
 app.use((err, req, res, next) => {
-  console.log(err);
+  console.error(err);
   res.status(err.statusCode || 500).json({ error: err.message });
 });
 
 app.listen(3000);
-
-// Next.jsによるルーティングのためこれ以降を追記
-const next = require("next");
-const dev = process.env.NODE_ENV !== "production";
-const nextApp = next({ dev });
-
-nextApp.prepare().then(
-  // pagesディレクトリ内の各Reactコンポーネントに対するサーバサイドルーティング
-  () => app.get("*", nextApp.getRequestHandler()),
-  (err) => {
-    console.error(err);
-    process.exit(1);
-  }
-);

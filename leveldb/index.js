a44c9a773cbdd86995f388e95dd100313bab0dcd
Yana <masatomo.yanagihara.jp@gmail.com>
2020-12-07 17:08:35 +0900

    Fix: ソースコードコピー

--- leveldb/index.js
+++ leveldb/index.js
@@ -3,8 +3,6 @@ const level = require("level");
 const { join } = require("path");
 // 同じディレクトリ内のleveldbディレクトリにデータベースの状態を保存
 const db = level(join(__dirname, "leveldb"));
-
-/** fetchAll()の実装 **/
 exports.fetchAll = async () => {
   const result = [];
   for await (const v of db.createValueStream({ gt: "todo:", lt: "todo;" })) {
@@ -12,31 +10,25 @@ exports.fetchAll = async () => {
   }
   return result;
 };
-
-/** fetchByCompleted()の実装 **/
 exports.fetchByCompleted = async (completed) => {
   const promises = [];
   for await (const id of db.createValueStream({
-    // セカンダリインデックスの検索
+    // 二次インデックスの検索
     gt: `todo-completed-${completed}:`,
     lt: `todo-completed-${completed};`,
   })) {
-    promises.push(db.get(`todo:${id}`.then(JSON.parse)));
+    promises.push(db.get(`todo:${id}`).then((value) => JSON.parse(value)));
   }
   return Promise.all(promises);
 };
-
-/** create()の実装 **/
 exports.create = (todo) =>
   db
     .batch()
     // ToDoの保存
     .put(`todo:${todo.id}`, JSON.stringify(todo))
-    // セカンダリインデックスの保存
-    .put(`todo-completed-${todo.completed}:${todo.id}, todo.id`)
+    // 二次インデックスの保存
+    .put(`todo-completed-${todo.completed}:${todo.id}`, todo.id)
     .write();
-
-/** update()の実装 **/
 exports.update = (id, update) =>
   db.get(`todo:${id}`).then(
     (content) => {
@@ -46,19 +38,17 @@ exports.update = (id, update) =>
         ...update,
       };
       let batch = db.batch().put(`todo:${id}`, JSON.stringify(newTodo));
-      // completedの値が変化した場合は、セカンダリインデックスも操作する
+      // completedの値が変化した場合は、二次インデックスも操作する
       if (oldTodo.completed !== newTodo.completed) {
         batch = batch
           .del(`todo-completed-${oldTodo.completed}:${id}`)
           .put(`todo-completed-${newTodo.completed}:${id}`, id);
       }
-      return batch.write();
+      return batch.write().then(() => newTodo);
     },
     // ToDoが存在しない場合はnullを返し、それ以外はそのままエラーにする
     (err) => (err.notFound ? null : Promise.reject(err))
   );
-
-/** remove()の実装 **/
 exports.remove = (id) =>
   db.get(`todo:${id}`).then(
     (content) =>
--- sqlite/index.js
+++ sqlite/index.js
@@ -1,16 +1,16 @@
 "use strict";
 const { promisify } = require("util");
-const { join, resolve } = require("path");
-const { rejects } = require("assert");
+const { join } = require("path");
 const sqlite3 =
   process.env.NODE_ENV === "production"
     ? require("sqlite3")
-    : require("sqlite3").verbose(); // production環境以外は冗長モードを利用
+    : // production環境以外は冗長モードを利用
+      require("sqlite3").verbose();
 
 // todo-data-storage/sqlite/sqliteというファイルにデータベースの状態を保存
 const db = new sqlite3.Database(join(__dirname, "sqlite"));
 
-// コールバックパターンのメソッドをPromise化
+// コールバックスタイルのメソッドをPromise化
 const dbGet = promisify(db.get.bind(db));
 const dbRun = function () {
   return new Promise((resolve, reject) =>
@@ -23,8 +23,6 @@ const dbRun = function () {
   );
 };
 const dbAll = promisify(db.all.bind(db));
-
-/** CREATE TABLE文の実行 **/
 dbRun(`CREATE TABLE IF NOT EXISTS todo (
   id TEXT PRIMARY KEY,
   title TEXT NOT NULL,
@@ -34,41 +32,32 @@ dbRun(`CREATE TABLE IF NOT EXISTS todo (
   console.error(err);
   process.exit(1);
 });
-
-/** fetchAll()の実装 **/
 // データベースのデータをToDoオブジェクトに変換する
-function rowToToDo(row) {
-  // SQLiteではBOOLEAN型の値が0, 1で保存される為、NOT演算子を２つ重ねて真偽値に変換する
+function rowToTodo(row) {
   return { ...row, completed: !!row.completed };
 }
 
 exports.fetchAll = () =>
-  dbAll("SELECT * FROM todo").then((rows) => rows.map(rowToToDo));
-
-/** fetchByCompleted()の実装 **/
+  dbAll("SELECT * FROM todo").then((rows) => rows.map(rowToTodo));
 exports.fetchByCompleted = (completed) =>
   dbAll("SELECT * FROM todo WHERE completed = ?", completed).then((rows) =>
-    rows.map(rowToToDo)
+    rows.map(rowToTodo)
   );
-
-/** create()の実装 **/
 exports.create = async (todo) => {
   await dbRun(
-    "INSERT INTO todo VALUES(?, ?, ?)",
+    "INSERT INTO todo VALUES (?, ?, ?)",
     todo.id,
     todo.title,
     todo.completed
   );
 };
-
-/** update()の実装 **/
 exports.update = (id, update) => {
   const setColumns = [];
   const values = [];
   for (const column of ["title", "completed"]) {
     if (column in update) {
       setColumns.push(`${column} = ?`);
-      values.push(`update[column]`);
+      values.push(update[column]);
     }
   }
   values.push(id);
@@ -77,12 +66,10 @@ exports.update = (id, update) => {
     values
   ).then(({ changes }) =>
     changes === 1
-      ? dbGet("SELECT * FROM todo WHERE id = ?", id).then(rowToToDo)
+      ? dbGet("SELECT * FROM todo WHERE id = ?", id).then(rowToTodo)
       : null
   );
 };
-
-/** remove()の実装 **/
 exports.remove = (id) =>
   dbRun("DELETE FROM todo WHERE id = ?", id).then(({ changes }) =>
     changes === 1 ? id : null
